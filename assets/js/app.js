6ac3dc362287619f9876b0159780904d78ec36f8
thomasvaeth <thomas.vaeth@gmail.com>
2016-09-04 19:05:21 -0700

    Added tab function. Need to change categories layout.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,98 @@
-var s,
-Preview = {
+var Tabs = (function() {
+  var s;
 
-  settings: {
-    img: document.getElementsByClassName('preview__img'),
-    post: document.getElementsByClassName('preview')
-  },
+  return {
+    settings: {
+      tabs: document.getElementsByClassName('tabs')[0].getElementsByTagName('li'),
+      tab: document.getElementsByClassName('tab')
+    },
 
-  init: function() {
-    s = this.settings;
-    this.display();
-    this.mouseenter();
-  },
+    init: function() {
+      s = this.settings;
+      this.display();
+      this.click();
+    },
 
-  display: function() {
-    [].forEach.call(s.img, function(img) {
-      img.style.display = 'none';
-    });
-    s.img[0].style.display = 'block';
-  },
+    display: function() {
+      if (s.tab.length) {
+        [].forEach.call(s.tab, function(tab) {
+          tab.style.display = 'none';
+        });
+        s.tab[0].style.display = 'block';
+        s.tab[0].classList.add('active');
+      }
+    },
 
-  mouseenter: function() {
-    var currentIdx = 0,
-        prevIdx = currentIdx;
+    click: function() {
+      if (s.tabs.length) {
+        var currentIdx = 0,
+            prevIdx = currentIdx;
 
-    [].forEach.call(s.post, function(preview, idx) {
-      preview.addEventListener('mouseenter', function() {
-        prevIdx = currentIdx;
-        currentIdx = idx;
+        [].forEach.call(s.tabs, function(tab, idx) {
+          tab.addEventListener('click', function() {
+            prevIdx = currentIdx;
+            currentIdx = idx;
 
-        if (prevIdx !== currentIdx) {
-          s.img[prevIdx].style.display = 'none';
-          s.img[currentIdx].style.display = 'block';
-        }
-      });
-    });
+            if (prevIdx !== currentIdx) {
+              s.tab[prevIdx].style.display = 'none';
+              s.tab[prevIdx].classList.remove('active');
+              s.tab[currentIdx].style.display = 'block';
+              s.tab[currentIdx].classList.add('active');
+            }
+          });
+        });
+      }
+    }
+
   }
-};
+})();
+
+var Preview = (function() {
+  var s;
+
+  return {
+    settings: {
+      img: document.getElementsByClassName('preview__img'),
+      post: document.getElementsByClassName('preview')
+    },
+
+    init: function() {
+      s = this.settings;
+      this.display();
+      this.mouseenter();
+    },
+
+    display: function() {
+      if (s.img.length) {
+        [].forEach.call(s.img, function(img) {
+          img.style.display = 'none';
+        });
+        s.img[0].style.display = 'block';
+      }
+    },
+
+    mouseenter: function() {
+      if (s.post.length) {
+        var currentIdx = 0,
+            prevIdx = currentIdx;
+
+        [].forEach.call(s.post, function(preview, idx) {
+          preview.addEventListener('mouseenter', function() {
+            prevIdx = currentIdx;
+            currentIdx = idx;
+
+            if (prevIdx !== currentIdx) {
+              s.img[prevIdx].style.display = 'none';
+              s.img[currentIdx].style.display = 'block';
+            }
+          });
+        });
+      }
+    }
+  }
+})();
 
 document.addEventListener('DOMContentLoaded', function() {
+  Tabs.init();
   Preview.init();
 });
